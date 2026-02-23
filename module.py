fcea698e619c2c0e3333da5558449bb6be315e3d
ioan beilic <ioan@ioans-MacBook-Pro.local>
2026-02-24 00:24:10 +0100

    fix: remove leading indentation from module.py

--- module.py
+++ module.py
@@ -1,31 +1,31 @@
-    from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
-    MODULE_ID = 'file_manager'
-    MODULE_NAME = _('File Manager')
-    MODULE_VERSION = '1.0.0'
-    MODULE_ICON = 'folder-open-outline'
-    MODULE_DESCRIPTION = _('File storage, organization and sharing')
-    MODULE_AUTHOR = 'ERPlora'
-    MODULE_CATEGORY = 'documents'
+MODULE_ID = 'file_manager'
+MODULE_NAME = _('File Manager')
+MODULE_VERSION = '1.0.0'
+MODULE_ICON = 'folder-open-outline'
+MODULE_DESCRIPTION = _('File storage, organization and sharing')
+MODULE_AUTHOR = 'ERPlora'
+MODULE_CATEGORY = 'documents'
 
-    MENU = {
-        'label': _('File Manager'),
-        'icon': 'folder-open-outline',
-        'order': 72,
-    }
+MENU = {
+    'label': _('File Manager'),
+    'icon': 'folder-open-outline',
+    'order': 72,
+}
 
-    NAVIGATION = [
-        {'label': _('Dashboard'), 'icon': 'speedometer-outline', 'id': 'dashboard'},
+NAVIGATION = [
+    {'label': _('Dashboard'), 'icon': 'speedometer-outline', 'id': 'dashboard'},
 {'label': _('Files'), 'icon': 'folder-open-outline', 'id': 'files'},
 {'label': _('Settings'), 'icon': 'settings-outline', 'id': 'settings'},
-    ]
+]
 
-    DEPENDENCIES = []
+DEPENDENCIES = []
 
-    PERMISSIONS = [
-        'file_manager.view_file',
+PERMISSIONS = [
+    'file_manager.view_file',
 'file_manager.add_file',
 'file_manager.change_file',
 'file_manager.delete_file',
 'file_manager.manage_settings',
-    ]
+]
